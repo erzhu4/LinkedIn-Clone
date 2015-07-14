fe2ee14088d255bfa5013f48f8d4837119b660d3
Eric Zhu <erzhu4@gmail.com>
2015-07-14 14:32:38 -0400

    Personal Info and stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,17 @@
 class SessionsController < ApplicationController
 
-  def new
-  end
-
   def create
     @user = User.find_by_creds(params[:email], params[:password])
     if @user
       login(@user)
       redirect_to "/user/home"
     else
-      render :new
+      render "root/root"
     end
   end
 
   def destroy
-    if(self.current_user)
+    if self.current_user
       self.current_user.reset_token
       self.current_user.save
       session[:token] = nil
