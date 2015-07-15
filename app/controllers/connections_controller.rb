62b34e702d91c2fdb0797fd7d4c201b753207dd0
Eric <erzhu4@gmail.com>
2015-07-15 15:22:50 -0400

    Users can now connect with each other.

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,6 +1,11 @@
 class ConnectionsController < ApplicationController
 
   def create
+    @connection1 = Connection.new({ user_id1: params[:id1], user_id2: params[:id2] })
+    @connection2 = Connection.new({ user_id1: params[:id2], user_id2: params[:id1] })
+    @connection1.save
+    @connection2.save
+    render text: "something"
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
-  resources :connections, only: [:create, :destroy]
   get "/user/home", to: "users#home"
   get "/current", to: "users#user", defaults: {format: :json}
   get "/user/:id", to: "users#profile_user", defaults: {format: :json}
   put "/current", to: "users#update_current"
   post "request/:id", to: "requests#create"
   post "/request/destroy/:senderid/:responderid", to: "requests#destroy"
+  post "/connections/:id1/:id2", to: "connections#create"
   root "site#root"
 end
