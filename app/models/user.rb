12e107ae8aac9a167749c1c936500955d2eb8f22
Eric <erzhu4@gmail.com>
2015-07-23 12:19:46 -0400

    s

--- app/models/user.rb
+++ app/models/user.rb
@@ -70,14 +70,12 @@ class User < ActiveRecord::Base
 
 def self.find_or_create_by_auth_hash(auth_hash)
   user = User.find_by(
-          # provider: auth_hash[:provider],
-          provider: "twitter",
+          provider: auth_hash[:provider],
           uid: auth_hash[:uid])
 
   unless user
     user = User.create!(
-          # provider: auth_hash[:provider],
-          provier: "twitter",
+          provider: auth_hash[:provider],
           uid: auth_hash[:uid],
           fname: auth_hash[:info][:name].split.first,
           lname: auth_hash[:info][:name].split.last,
