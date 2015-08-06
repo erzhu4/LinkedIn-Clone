51fd7480804269256df1f133a3719733b89cc1fb
Eric <erzhu4@gmail.com>
2015-08-06 16:26:29 -0400

    Search query simplified

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,15 @@
 class SearchController < ApplicationController
 
   def get_results ## Possibility of SQL injections
-    @results1 = []
-    @results2 = []
+    @results = []
     str = params[:str]
     cap_str = params[:str].capitalize;
-    if str && cap_str
-      @results1 = User.where("users.fname LIKE '%" + cap_str + "%' OR users.lname LIKE '%" + cap_str +"%' OR users.email LIKE '%" + cap_str + "'" );
-      @results2 = User.where("users.fname LIKE '%" + str + "%' OR users.lname LIKE '%" + str +"%' OR users.email LIKE '%" + str + "'" );
+    if str && cap_str.length > 0
+      @results = User.where("users.fname LIKE '%" + cap_str + "%' OR users.lname LIKE '%" + cap_str +"%' OR users.email LIKE '%" + cap_str + "' OR users.fname LIKE '%" + str + "%' OR users.lname LIKE '%" + str +"%' OR users.email LIKE '%" + str + "'");
+    else
+      @results = []
     end
-    render json: @results1 + @results2
+    render json: @results
   end
 
 end
